09301ddd141e2f749ee10cde90b9d2f0928c85ed
Danyil Marchuk <danmarchuk9@gmail.com>
2023-02-01 17:51:55 +0000

    Revert "Merge branch 'dev' into 'main'"
    This reverts merge request !1

--- Reverse/ViewController.swift
+++ Reverse/ViewController.swift
@@ -9,48 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var progressBar: UIProgressView!
+
     @IBOutlet weak var reversedTextLabel: UILabel!
     @IBOutlet weak var userInputTextView: UITextField!
-    
-    @IBOutlet weak var changeButtonText: UIButton!
-    
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        progressBar.progress = 0.0
-        changeButtonText.setTitle("Reverse", for: .normal)
-        userInputTextView.delegate = self
-        reversedTextLabel.textColor = UIColor.systemBlue
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func reverseButton(_ sender: UIButton) {
+        if let textToReverse = userInputTextView.text {
+            
+            var result = ""
+            
+            let separateWords = textToReverse.components(separatedBy: " ")
+            
+            
+            for aWord in separateWords {
+                let reversedWordsWithSpace = String(aWord.reversed() + " ")
+                result.append(reversedWordsWithSpace)
+                reversedTextLabel.text = result
+                userInputTextView.text = ""
+            }
+        }
+        
     }
     
-    @IBAction func reverseButton(_ sender: UIButton) {
-        
-        if (changeButtonText.currentTitle == "Reverse") {
-            if let textToReverse = userInputTextView.text {
-                let separateWords = textToReverse.components(separatedBy: " ")
-                reversedTextLabel.text = String(separateWords.map {$0.reversed()}.joined(separator: " "))
-                changeButtonText.setTitle("Clear", for: .normal)}
-            
-        } else if (changeButtonText.currentTitle == "Clear")   {
-            reversedTextLabel.text = ""
-            userInputTextView.text = ""
-            changeButtonText.setTitle("Reverse", for: .normal)
-            progressBar.progress = 0.0
-            userInputTextView.endEditing(true)
-        }
-        
-    }
-}
-
-// MARK: - UITextFieldDelegate
-extension ViewController: UITextFieldDelegate {
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField == userInputTextView {
-            progressBar.progress = 1.0
-            userInputTextView.becomeFirstResponder()
-        }
-    }
 }
