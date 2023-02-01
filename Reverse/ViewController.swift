b248e33664a4ce8caeca582aabde3c603e6f56eb
Danyil Marchuk <zemero58@gmail.com>
2023-02-01 18:40:32 +0100

    1) Get rid of the frame of the userInputTextView 2) Add a progress bar that will become blue once the user tapped on it 3) Depending on the label of the button either reverse a word or clean the views 4) When the clean button is pressed the keyboard is dismissed and the progress bar becomes grey again
    (cherry picked from commit 51cb9ded299950baa9f6577cf7ede63ca7bf8977)

--- Reverse/ViewController.swift
+++ Reverse/ViewController.swift
@@ -9,23 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var reversedTextLabel: UILabel!
     @IBOutlet weak var userInputTextView: UITextField!
+    
+    @IBOutlet weak var changeButtonText: UIButton!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    @IBAction func reverseButton(_ sender: UIButton) {
-        if let textToReverse = userInputTextView.text {
-            let separateWords = textToReverse.components(separatedBy: " ")
-            reversedTextLabel.text = String(separateWords.map {$0.reversed()}.joined(separator: " "))
-            userInputTextView.text = ""
-                
-            }
-
-        }
+        progressBar.progress = 0.0
+        changeButtonText.setTitle("Reverse", for: .normal)
+        userInputTextView.delegate = self
         
     }
     
+    @IBAction func reverseButton(_ sender: UIButton) {
+        
+        
+        if (changeButtonText.currentTitle == "Reverse") {
+            if let textToReverse = userInputTextView.text {
+                let separateWords = textToReverse.components(separatedBy: " ")
+                reversedTextLabel.text = String(separateWords.map {$0.reversed()}.joined(separator: " "))
+                changeButtonText.setTitle("Clear", for: .normal)}
+            
+        } else if (changeButtonText.currentTitle == "Clear")   {
+            reversedTextLabel.text = ""
+            userInputTextView.text = ""
+            changeButtonText.setTitle("Reverse", for: .normal)
+            progressBar.progress = 0.0
+            userInputTextView.endEditing(true)
+        }
+        
+    }
+}
 
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == userInputTextView {
+            progressBar.progress = 1.0
+            userInputTextView.becomeFirstResponder()
+        }
+    }
+}
